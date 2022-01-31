d42d1e76589bba8f63eb067903a477ca49b46472
txcpnae <johol842@student.liu.se>
2022-01-31 18:40:10 +0100

    Added file generator for random cars. Call it by ./a.out NUM where NUM is number of rows you want generated. Is compiled seperatly by w++17, as i use more modern c++ than the rest of the project

---
+++ Traffic/random_gen.cc
@@ -0,0 +1,28 @@
+#include <random>
+#include <fstream>
+#include <cassert>
+
+using std::ofstream;
+
+int main(int argc, char **argv)
+{
+
+    assert( argc == 2); // 1 inputs
+    int rows = atof(argv[1]);
+
+    ofstream out;
+    out.open( "input/random_input.txt");
+
+    bool rand_val;
+    for(int i{}; i<rows; i++)
+    {
+        for(int j{}; j<4; j++)
+        {
+            rand_val = rand() & 0x01;
+            out << rand_val << " ";
+        }
+        out << "\n";
+    }
+
+    out.close();
+}
