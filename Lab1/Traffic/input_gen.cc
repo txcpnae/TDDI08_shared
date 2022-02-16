3af165449052bbbf04129f643455d86d10e31d51
txcpnae <johol842@student.liu.se>
2022-02-16 19:08:24 +0100

    Wierd Mode Change- Commited

--- Lab1/Traffic/input_gen.cc
+++ Lab1/Traffic/input_gen.cc
@@ -32,11 +32,12 @@ void Generator::generate_thread()
 
         for(int i{}; i<4; i++)
         {
-            cars[i]->write(cars[i]->read() + n[i]);
+            //cars[i]->write(cars[i]->read() + n[i]);
+            cars[i]->write(n[i]);
             if(lights[i] == true)
             {
                 //would be cool to add a wait() to simulate time for a car to drive through intersection
-                cars[i]->write(false);
+                //cars[i]->write(false);
             }
 
         }
